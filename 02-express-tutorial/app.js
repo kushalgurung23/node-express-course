83ef56754f3efae1febb5086c19f2aeba714b5c4
Kushal Gurung <qoosal23.kg@gmail.com>
2023-03-07 00:21:55 +0800

    express start

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,10 +1,30 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'content-type': 'text/html'})
-    res.write('<h1>KUSHAL GURUNG </h1>');
-    res.end();
+app.get('/', (req, res) => {
+    console.log('hit resource');
+    res.status(200).send('Home page');
 });
 
-server.listen(5000);
-// 4:20:13
+app.get('/about', (req, res) => {
+    res.status(200).send('ABOUT PAGE');
+});
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>resource not found</h1>')
+});
+
+app.listen(3000, () => {
+    console.log("3000 server has started");
+});
+
+// 5:03:11
+// npm install express@4.17.1 --save
+
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
