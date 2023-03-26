6bbd2c7b941c81fe479fba549106b12b2187131b
Kushal Gurung <qoosal23.kg@gmail.com>
2023-03-26 15:28:33 +0800

    Basic jwt complete

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,10 +1,11 @@
-const CustomAPIError = require('../errors/custom-error')
+const {BadRequestError} = require('../errors')
 const jwt = require('jsonwebtoken')
+const {StatusCodes} = require('http-status-codes')
 
 const login  = async (req, res) => {
    const {username, password} = req.body;
    if(!username || !password) {
-    throw new CustomAPIError('Please provide both email and password', 400)
+    throw new BadRequestError('Please provide both email and password')
    }
 
    const id = new Date().getDate()
@@ -18,8 +19,9 @@ const login  = async (req, res) => {
 }
 
 const dashboard = async (req, res) => {
+    const {id, username } = req.user;
     const luckyNumber = Math.floor(Math.random() * 100)
-    res.status(200).json({msg: `Hello, Gurung Kushal`, secret: `Here is your authorized data. Your lucky number is ${luckyNumber}`})
+    res.status(StatusCodes.OK).json({msg: `Hello, ${username}`, secret: `Here is your authorized data. Your lucky number is ${luckyNumber}`})
 } 
 
 module.exports = {
